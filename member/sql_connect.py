6d6c2ed70c87de3adc16850e525e6eeec1db4a7b
nsw2022 <tmddn3410@naver.com>
2023-05-02 12:25:18 +0900

    05_02

---
+++ member/sql_connect.py
@@ -0,0 +1,30 @@
+import  sqlite3
+
+def getconn():
+    conn = sqlite3.connect('E:/NSW/sqlworks/pydb/member.db')
+    return conn
+
+def select():
+    conn = getconn()
+    cursor = conn.cursor()
+    sql = "SELECT * FROM member"
+    cursor.execute(sql)
+    rs = cursor.fetchall()
+    print(rs)
+    for i in rs:
+        print(i)
+    conn.close()
+
+def insert():
+    conn = getconn()
+    cursor = conn.cursor()
+
+    sql = "INSERT INTO member(memberid, password, name, gender)" \
+          "VALUES(?,?,?,?)"
+    cursor.execute(sql, ('today123', 'm123456$', '투데이', '여'))
+    conn.commit()
+    conn.close()
+
+
+insert()
+select()
--- member/start_app.py
+++ member/start_app.py
@@ -2,15 +2,64 @@
 # templates 폴더, static
 # 웹 서버 - flask
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
 
 app = Flask(__name__)
+
+def getconn():
+    conn = sqlite3.connect('E:/NSW/sqlworks/pydb/member.db')
+    return conn
 
 # url - '/' 설정
 @app.route('/')
 def index():
     return render_template("index.html")
+
+#회원 목록 methods는 기본값이 GET이라 GET방식일땐 생략해도 가능
+@app.route('/memberlist', methods = ['GET'])
+def memberlist():
+
+    conn = getconn()
+    cursor = conn.cursor()
+    sql = "SELECT * FROM member"
+    cursor.execute(sql)
+    rs = cursor.fetchall()
+    conn.close()
+
+    return render_template('memberlist.html', rs=rs)
+
+# 회원가입
+@app.route('/register', methods = ['GET','POST'])
+def register():
+    if request.method == 'POST':
+        # 가입 폼에 입력된 자료 넘겨 받음
+        id = request.form['memberid']
+        pw = request.form['passwd1']
+        name = request.form['name']
+        gender = request.form['gender']
+
+        # db에 저장
+        conn = getconn()
+        cursor = conn.cursor()
+        sql = f"INSERT INTO member(memberid, password, name, gender) " \
+              f" VALUES('{id}', '{pw}', '{name}', '{gender}')"
+        cursor.execute(sql)
+        conn.commit()
+        conn.close()
+
+        # 이방식은 안되려나? 나중에 테스트해보기 안되네
+        # sql = "INSERT INTO member(memberid, password, name, gender) " \
+        #       " VALUES(?,?,?,?)"
+        # cursor.execute(sql, (id,pw,name,gender))
+        # conn.commit()
+        # conn.close()
+
+        # redirect - 회원 가입후 회원 목록 페이지로 강제로 이동
+        return redirect(url_for('memberlist'))
+    else:
+        return render_template('register.html')
 
 
 app.run()
